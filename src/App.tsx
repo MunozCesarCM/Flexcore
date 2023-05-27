7c2ca4df5a2853c5d7e54867e7acf22afabd4b23
MunozCesarCM <munozcesar.cm@gmail.com>
2023-05-27 13:50:48 -0600

    Implemented click scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import CodeMirror from '@uiw/react-codemirror';
 import { html } from '@codemirror/lang-html';
 import Sidebar from './components/Sidebar';
@@ -6,7 +6,10 @@ import { TbArrowBigLeftLinesFilled, TbArrowBigRightLinesFilled } from 'react-ico
 import {  FiMoreVertical, FiCopy, FiRotateCcw } from 'react-icons/fi';
 
 function App() {
+  const sampleRef = useRef<null | HTMLDivElement>(null);
+
   const [sidebarActive, setSidebarActive] = useState(true);
+
   return (
     <main>
       <Sidebar
@@ -86,6 +89,70 @@ Link
                 />
               </div>
             </div>
+            <p>Buttons allow users to take actions, and make choices, with a single tap.</p>
+            <p>Buttons communicate <span className='quote'>actions</span> that users can take. They are typically placed throughout your UI, in places like:</p>
+            <ul>
+              <li>Modal Windows</li>
+              <li>Forms</li>
+              <li>Cards</li>
+              <li>Toolbars</li>
+            </ul>
+            <h3 ref={sampleRef}>Customization</h3>
+            <p>The <span className='quote'>Button</span> comes with three variants: text (default), contained, and outlined.</p>
+            <div className='code-result'>
+              <button style={{margin: '0 1em', background: 'none', cursor: 'pointer', border: 'none', color: '#1565c0', padding: '0.75em 2em', borderRadius: '3px'}}>Text</button>
+              <button style={{margin: '0 1em', background: '#1565c0', cursor: 'pointer', border: 'none', color: 'white', padding: '0.75em 2em', borderRadius: '3px'}}>Contained</button>
+              <button style={{margin: '0 1em', background: 'none', border: '1px solid #1565c0', cursor: 'pointer', color: '#1565c0', padding: '0.75em 2em', borderRadius: '3px'}}>Outlined</button>
+            </div>
+            <div className='code-container'>
+              <ul>
+                <li><FiRotateCcw /></li>
+                <li><FiCopy /></li>
+                <li><FiMoreVertical /></li>
+              </ul>
+              <div className='code-example'>
+                <CodeMirror
+                  value={`<button variant="text">Text</button>
+<button variant="contained">Contained</button>
+<button variant="outlined">Outlined</button>`}
+                  theme="dark"
+                  extensions={html()}
+                />
+              </div>
+            </div>
+            <hr />
+            <h4>Text Button</h4>
+            <p><span className='underline'>Text buttons</span> are typically used for less-pronounced actions, including those located in:</p>
+            <ul>
+              <li>Dialogs</li>
+              <li>Cards</li>
+            </ul>
+            <p>In cards, text buttons help maintain an emphasis on card content.</p>
+            <div className='code-result'>
+              <button style={{margin: '0 1em', background: '#1565c0', cursor: 'pointer', border: 'none', color: 'white', padding: '0.75em 2em', borderRadius: '3px'}}>One</button>
+              <button style={{margin: '0 1em', background: '#1565c0', cursor: 'pointer', border: 'none', color: 'white', padding: '0.75em 2em', borderRadius: '3px'}}>Two</button>
+              <button style={{margin: '0 1em', background: '#1565c0', cursor: 'pointer', border: 'none', color: 'white', padding: '0.75em 2em', borderRadius: '3px'}}>Three</button>
+            </div>
+            <div className='code-container'>
+              <ul>
+                <li><FiRotateCcw /></li>
+                <li><FiCopy /></li>
+                <li><FiMoreVertical /></li>
+              </ul>
+              <div className='code-example'>
+                <CodeMirror
+                  value={`<Button variant="contained">Contained</Button>
+<Button variant="contained" disabled>
+Disabled
+</Button>
+<Button variant="contained" href="#contained-buttons">
+Link
+</Button>`}
+                  theme="dark"
+                  extensions={html()}
+                />
+              </div>
+            </div>
           </section>
         </div>
         <div className='sidebar'>
@@ -105,7 +172,7 @@ Link
           <h5>Sizes</h5>
           <h5>Colors</h5>
 
-          <h4>Customization</h4>
+          <h4 onClick={() => sampleRef.current?.scrollIntoView({behavior: 'smooth'})}>Customization</h4>
           <h4>Complex Button</h4>
         </div>
       </article>

11a78a0ab6e24b5d22f6790e0ad447194a1b94c1
MunozCesarCM <munozcesar.cm@gmail.com>
2023-06-07 12:40:08 -0600

    Moved styles to public

--- src/components/CodeExample.tsx
+++ src/components/CodeExample.tsx
@@ -2,7 +2,6 @@ import { useState, useRef, useContext } from 'react';
 import CodeMirror from '@uiw/react-codemirror';
 import { html } from '@codemirror/lang-html';
 import { FiSun, FiCopy, FiRotateCcw } from 'react-icons/fi';
-import { fleXkitCSS } from '../constants/fleXkit';
 import AppContext from '../context/AppContext';
 
 const formatCodeExample = (snippet: string, template: boolean, theme: string) => {
@@ -14,9 +13,10 @@ const formatCodeExample = (snippet: string, template: boolean, theme: string) =>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="/flexcore.css">
 <style>
 * { font-family: 'Poppins', sans-serif; }
-${fleXkitCSS}
+body { padding: 1em 2em; }
 </style>
 </head>
 <body class="theme-${theme}">
@@ -32,9 +32,10 @@ ${snippet}
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="../flexcore.css">
 <style>
 * { font-family: 'Poppins', sans-serif; }
-${fleXkitCSS}
+body { padding: 1em 2em; }
 </style>
 </head>
 ${snippet}

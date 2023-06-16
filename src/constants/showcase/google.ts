d101fb9ffa11750e5533bf4291a6f0a8a0d92e94
MunozCesarCM <munozcesar.cm@gmail.com>
2023-06-16 17:19:24 -0600

    Changed Google Drive icons

--- src/constants/showcase/google.ts
+++ src/constants/showcase/google.ts
@@ -138,37 +138,37 @@ export const example3 = `
       <img class="w-10 ml-1" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Google_Drive_logo.png/240px-Google_Drive_logo.png" />
       <span class="text-xl text-neutral-500">Drive</span>
     </header>
-    <button class="bg-none hover:bg-neutral-50 border border-neutral-200 rounded-full mt-10 px-7 py-3 flex items-center gap-5 cursor-pointer duration-50">
-      <svg class="w-5 cursor-pointer" fill="#535E69" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h4v4H0V0zm0 6h4v4H0V6zm0 6h4v4H0v-4zM6 0h4v4H6V0zm0 6h4v4H6V6zm0 6h4v4H6v-4zm6-12h4v4h-4V0zm0 6h4v4h-4V6zm0 6h4v4h-4v-4z" fill-rule="evenodd"/></svg>
+    <button class="bg-none hover:bg-neutral-50 border border-neutral-200 rounded-full mt-10 px-7 py-3 flex items-center gap-3 cursor-pointer duration-50">
+      <svg class="w-6 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#535E69" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
       <span class="text-neutral-500 weight-500">New</span>
     </button>
     <div class="weight-500 text-xs mt-5 mr-5 py-3 px-5 flex items-center gap-5 hover:bg-neutral-100 rounded-r-full cursor-pointer">
-      <svg class="w-3 cursor-pointer" fill="#535E69" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h4v4H0V0zm0 6h4v4H0V6zm0 6h4v4H0v-4zM6 0h4v4H6V0zm0 6h4v4H6V6zm0 6h4v4H6v-4zm6-12h4v4h-4V0zm0 6h4v4h-4V6zm0 6h4v4h-4v-4z" fill-rule="evenodd"/></svg>
+      <svg class="w-4 cursor-pointer" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3 12C3 4.5885 4.5885 3 12 3C19.4115 3 21 4.5885 21 12C21 19.4115 19.4115 21 12 21C4.5885 21 3 19.4115 3 12Z" stroke="#535E69" stroke-width="2"/><path d="M9 12L10.6828 13.6828V13.6828C10.858 13.858 11.142 13.858 11.3172 13.6828V13.6828L15 10" stroke="#535E69" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>
       <span>Priority</span>
     </div>
     <div class="bg-sky-50 text-blue-500 weight-600 text-xs mt-2 mr-5 py-3 px-5 rounded-r-full flex items-center gap-5 cursor-pointer">
-      <svg class="w-3 cursor-pointer" fill="#3B82F6" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h4v4H0V0zm0 6h4v4H0V6zm0 6h4v4H0v-4zM6 0h4v4H6V0zm0 6h4v4H6V6zm0 6h4v4H6v-4zm6-12h4v4h-4V0zm0 6h4v4h-4V6zm0 6h4v4h-4v-4z" fill-rule="evenodd"/></svg>
+      <svg class="w-4 cursor-pointer" version="1.0" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 64 64" enable-background="new 0 0 64 64" xml:space="preserve"><g><circle fill="#3B82F6" cx="32" cy="56" r="1"/><circle fill="#3B82F6" cx="42" cy="56" r="1"/><path fill="#3B82F6" d="M60,46V4c0-2.211-1.789-4-4-4H8C5.789,0,4,1.789,4,4v42H60z M32,6c9.941,0,18,8.059,18,18s-8.059,18-18,18 s-18-8.059-18-18S22.059,6,32,6z"/><circle fill="#3B82F6" cx="32" cy="24" r="16"/><path fill="#3B82F6" d="M4,48v12c0,2.211,1.789,4,4,4h48c2.211,0,4-1.789,4-4V48H4z M32,59c-1.657,0-3-1.344-3-3s1.343-3,3-3 s3,1.344,3,3S33.657,59,32,59z M42,59c-1.657,0-3-1.344-3-3s1.343-3,3-3s3,1.344,3,3S43.657,59,42,59z M52,59c-1.657,0-3-1.344-3-3 s1.343-3,3-3s3,1.344,3,3S53.657,59,52,59z"/><circle fill="#3B82F6" cx="52" cy="56" r="1"/></g></svg>
       <span>My Drive</span>
     </div>
     <div class="weight-500 text-xs mt-2 mr-5 py-3 px-5 flex items-center gap-5 hover:bg-neutral-100 rounded-r-full cursor-pointer">
-      <svg class="w-3 cursor-pointer" fill="#535E69" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h4v4H0V0zm0 6h4v4H0V6zm0 6h4v4H0v-4zM6 0h4v4H6V0zm0 6h4v4H6V6zm0 6h4v4H6v-4zm6-12h4v4h-4V0zm0 6h4v4h-4V6zm0 6h4v4h-4v-4z" fill-rule="evenodd"/></svg>
+      <svg class="w-4 cursor-pointer" version="1.0" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 64 64" enable-background="new 0 0 64 64" xml:space="preserve"><g><circle fill="#535E69" cx="32" cy="56" r="1"/><circle fill="#535E69" cx="42" cy="56" r="1"/><path fill="#535E69" d="M60,46V4c0-2.211-1.789-4-4-4H8C5.789,0,4,1.789,4,4v42H60z M32,6c9.941,0,18,8.059,18,18s-8.059,18-18,18 s-18-8.059-18-18S22.059,6,32,6z"/><circle fill="#535E69" cx="32" cy="24" r="16"/><path fill="#535E69" d="M4,48v12c0,2.211,1.789,4,4,4h48c2.211,0,4-1.789,4-4V48H4z M32,59c-1.657,0-3-1.344-3-3s1.343-3,3-3 s3,1.344,3,3S33.657,59,32,59z M42,59c-1.657,0-3-1.344-3-3s1.343-3,3-3s3,1.344,3,3S43.657,59,42,59z M52,59c-1.657,0-3-1.344-3-3 s1.343-3,3-3s3,1.344,3,3S53.657,59,52,59z"/><circle fill="#535E69" cx="52" cy="56" r="1"/></g></svg>
       <span>Team Drives</span>
     </div>
     <hr class="mr-5 border border-neutral-100" />
     <div class="weight-500 text-xs mt-2 mr-5 py-3 px-5 flex items-center gap-5 hover:bg-neutral-100 rounded-r-full cursor-pointer">
-      <svg class="w-3 cursor-pointer" fill="#535E69" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h4v4H0V0zm0 6h4v4H0V6zm0 6h4v4H0v-4zM6 0h4v4H6V0zm0 6h4v4H6V6zm0 6h4v4H6v-4zm6-12h4v4h-4V0zm0 6h4v4h-4V6zm0 6h4v4h-4v-4z" fill-rule="evenodd"/></svg>
+      <svg class="w-4 cursor-pointer" fill="#535E69" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#535E69" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
       <span>Shared with me</span>
     </div>
     <div class="weight-500 text-xs mt-2 mr-5 py-3 px-5 flex items-center gap-5 hover:bg-neutral-100 rounded-r-full cursor-pointer">
-      <svg class="w-3 cursor-pointer" fill="#535E69" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h4v4H0V0zm0 6h4v4H0V6zm0 6h4v4H0v-4zM6 0h4v4H6V0zm0 6h4v4H6V6zm0 6h4v4H6v-4zm6-12h4v4h-4V0zm0 6h4v4h-4V6zm0 6h4v4h-4v-4z" fill-rule="evenodd"/></svg>
+      <svg class="w-4 cursor-pointer" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g><g><g><polyline fill="none" points="11.9 5.9 11.9 11.9 12 12 14.1 14.1" stroke="#535E69" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/><circle cx="12" cy="12" data-name="Circle" fill="none" id="Circle-2" r="10" stroke="#535E69" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/></g></g></g></svg>
       <span>Recent</span>
     </div>
     <div class="weight-500 text-xs mt-2 mr-5 py-3 px-5 flex items-center gap-5 hover:bg-neutral-100 rounded-r-full cursor-pointer">
-      <svg class="w-3 cursor-pointer" fill="#535E69" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h4v4H0V0zm0 6h4v4H0V6zm0 6h4v4H0v-4zM6 0h4v4H6V0zm0 6h4v4H6V6zm0 6h4v4H6v-4zm6-12h4v4h-4V0zm0 6h4v4h-4V6zm0 6h4v4h-4v-4z" fill-rule="evenodd"/></svg>
+      <svg class="w-4 cursor-pointer" fill="#535E69" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#535E69" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>
       <span>Starred</span>
     </div>
     <div class="weight-500 text-xs mt-2 mr-5 py-3 px-5 flex items-center gap-5 hover:bg-neutral-100 rounded-r-full cursor-pointer">
-      <svg class="w-3 cursor-pointer" fill="#535E69" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h4v4H0V0zm0 6h4v4H0V6zm0 6h4v4H0v-4zM6 0h4v4H6V0zm0 6h4v4H6V6zm0 6h4v4H6v-4zm6-12h4v4h-4V0zm0 6h4v4h-4V6zm0 6h4v4h-4v-4z" fill-rule="evenodd"/></svg>
+      <svg class="w-4 cursor-pointer" fill="#535E69" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#535E69" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
       <span>Trash</span>
     </div>
     <hr class="mr-5 border border-neutral-100" />
@@ -243,7 +243,6 @@ export const example3 = `
           <span>Taxes</span>
         </div>
       </section>
-      <span class="weight-500 text-xs">Files</span>
       <section class="my-3 flex gap-2 pb-10">
         <section class="w-full text-xs">
           <header class="flex border-0 border-b border-neutral-200 py-2">
@@ -255,7 +254,7 @@ export const example3 = `
             <div class="hover:bg-neutral-100 flex text-neutral-400 weight-500 border-0 border-b border-neutral-200 py-3 cursor-pointer duration-50">
               <div class="w-3/5 text-neutral-500 flex items-center gap-4 pl-2">
                 <svg class="w-4" fill="#4885E9" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M13 2H6a2 2 0 0 0-2 2v16c0 1.1.9 2 2 2h12a2 2 0 0 0 2-2V9l-7-7z"/><path d="M13 3v6h6"/></svg>
-                <span>Team 2023 Planning Presentation</span>
+                <span class="flex items-center gap-3">Team 2023 Planning Presentation<svg class="w-4 cursor-pointer" fill="#535E69" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#535E69" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg></span>
               </div>
               <div class="w-1/5">Business Team</div>
               <div class="w-1/5">10:42 AM</div>
@@ -271,7 +270,7 @@ export const example3 = `
             <div class="hover:bg-neutral-100 flex text-neutral-400 weight-500 border-0 border-b border-neutral-200 py-3 cursor-pointer duration-50">
               <div class="w-3/5 text-neutral-500 flex items-center gap-4 pl-2">
                 <svg class="w-4" fill="#F8C101" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M13 2H6a2 2 0 0 0-2 2v16c0 1.1.9 2 2 2h12a2 2 0 0 0 2-2V9l-7-7z"/><path d="M13 3v6h6"/></svg>
-                <span>Nature Presentation</span>
+                <span class="flex items-center gap-3">Nature Presentation<svg class="w-4 cursor-pointer" fill="#535E69" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#535E69" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg></span>
               </div>
               <div class="w-1/5">me</div>
               <div class="w-1/5">Apr 7, 2023</div>
@@ -279,7 +278,7 @@ export const example3 = `
             <div class="hover:bg-neutral-100 flex text-neutral-400 weight-500 border-0 border-b border-neutral-200 py-3 cursor-pointer duration-50">
               <div class="w-3/5 text-neutral-500 flex items-center gap-4 pl-2">
                 <svg class="w-4" fill="#129F5B" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M3 3h18v18H3zM21 9H3M21 15H3M12 3v18"/></svg>
-                <span>Summit Wrapup Attendance Report</span>
+                <span class="flex items-center gap-3">Summit Wrapup Attendance Report<svg class="w-4 cursor-pointer" fill="#535E69" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#535E69" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg></span>
               </div>
               <div class="w-1/5">Summit Staff</div>
               <div class="w-1/5">Apr 6, 2023</div>

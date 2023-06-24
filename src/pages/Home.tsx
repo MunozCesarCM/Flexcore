793f5aff7582266d161a16f43ec5f8cd299a1659
MunozCesarCM <munozcesar.cm@gmail.com>
2023-06-24 12:39:47 -0600

    Allow build to host

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -27,62 +27,60 @@ const Home = () => {
   };
 
   return (
-    <article  className={ sidebarActive ? 'landing-page-sb-active' : 'landing-page'}>
-      <div className='content'>
-        <section className='landing' style={{maxWidth: (sidebarActive ? 'calc(100vw - 300px)' : '100vw')}}>
-          <header>
-            <section className='hero'>
-              <h1><span className='special'>Create</span> with Confidence using Intuitive <span className='special'>UI design.</span></h1>
-              <p>Craft <strong>stunning interfaces</strong> by customizing components with ease, and see your vision come to life. With Flexcore's focus on practicality and <strong>seamless integration</strong>, you can <strong>effortlessly</strong> build responsive and visually appealing websites.</p>
-              <div className='actions'>
-                <Link to='/getting-started'>
-                  <button className='primary'>
-                    <span>Get Started</span>
-                    <RiArrowDropRightLine />
-                  </button>
-                </Link>
-                <Link to='/showcase/google'>
-                  <button className='secondary'>
-                    <span>Showcase</span>
-                  </button>
-                </Link>
-              </div>
-            </section>
-            <section className='demo'>
-              <iframe
-                ref={ref}
-                onLoad={onLoad}
-                srcDoc={formatCode(example1, true, siteTheme, siteTheme, 'Poppins')}
-                title='output'
-                sandbox='allow-same-origin'
-                frameBorder='0'
-                style={{height: height, width: '55vw'}}
-              />
-            </section>
-          </header>
-          <section className='features'>
-            <div className='feature'>
-              <TbCategory2 />
-              <h2>Extensive Customization</h2>
-            </div>
-            <div className='feature'>
-              <TbMoonStars />
-              <h2>Built-in Dark Mode</h2>
-            </div>
-            <div className='feature'>
-              <TbPalette />
-              <h2>Effortless Theming</h2>
-            </div>
-            <div className='feature'>
-              <TbArrowAutofitWidth />
-              <h2>Responsive Design</h2>
+    <article  className='landing-page'>
+      <section className='landing'>
+        <header>
+          <section className='hero'>
+            <h1><span className='special'>Create</span> with Confidence using Intuitive <span className='special'>UI design.</span></h1>
+            <p>Craft <strong>stunning interfaces</strong> by customizing components with ease, and see your vision come to life. With Flexcore's focus on practicality and <strong>seamless integration</strong>, you can <strong>effortlessly</strong> build responsive and visually appealing websites.</p>
+            <div className='actions'>
+              <Link to='/getting-started'>
+                <button className='primary'>
+                  <span>Get Started</span>
+                  <RiArrowDropRightLine />
+                </button>
+              </Link>
+              <Link to='/showcase/google'>
+                <button className='secondary'>
+                  <span>Showcase</span>
+                </button>
+              </Link>
             </div>
           </section>
-          <section className='showcase'>
-            DEMOS
+          <section className='demo'>
+            <iframe
+              ref={ref}
+              onLoad={onLoad}
+              srcDoc={formatCode(example1, true, siteTheme, siteTheme, 'Poppins')}
+              title='output'
+              sandbox='allow-same-origin'
+              frameBorder='0'
+              style={{height: height, width: '55vw'}}
+            />
           </section>
+        </header>
+        <section className='features'>
+          <div className='feature'>
+            <TbCategory2 />
+            <h2>Extensive Customization</h2>
+          </div>
+          <div className='feature'>
+            <TbMoonStars />
+            <h2>Built-in Dark Mode</h2>
+          </div>
+          <div className='feature'>
+            <TbPalette />
+            <h2>Effortless Theming</h2>
+          </div>
+          <div className='feature'>
+            <TbArrowAutofitWidth />
+            <h2>Responsive Design</h2>
+          </div>
         </section>
-      </div>
+        <section className='showcase'>
+          DEMOS
+        </section>
+      </section>
     </article>
   );
 }

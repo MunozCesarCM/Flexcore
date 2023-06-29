c5a43139984bf4f976126a8ab1a5049ab9f9962f
MunozCesarCM <munozcesar.cm@gmail.com>
2023-06-29 07:46:48 -0600

    Fixed all typescript issues

--- src/components/ColorPalette.tsx
+++ src/components/ColorPalette.tsx
@@ -1,21 +1,44 @@
 import { useEffect } from 'react';
 
+interface ColorMap { [key: string]: string[] }
+interface ColorName { [key: string]: string }
+
+const colorValues = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950];
+const colorNames: ColorName = {
+  red: 'Red',
+  orange: 'Orange',
+  amber: 'Amber',
+  yellow: 'Yellow',
+  lime: 'Lime',
+  green: 'Green',
+  emerald: 'Emerald',
+  teal: 'Teal',
+  cyan: 'Cyan',
+  sky: 'Sky',
+  blue: 'Blue',
+  indigo: 'Indigo',
+  violet: 'Violet',
+  purple: 'Purple',
+  magenta: 'Magenta',
+  pink: 'Pink',
+  rose: 'Rose',
+  slate: 'Slate',
+  neutral: 'Neutral',
+};
+
 const ColorPalette = () => {
   useEffect(() => {
-    const buttons = document.querySelectorAll(".color-button");
+    const buttons = document.querySelectorAll('.color-button');
 
-    const selectColor = (name) => {
+    const selectColor = (name: string) => {
       const color = colors[name];
-      const palettes = document.querySelectorAll(".color-palette-item");
-      const hexes = document.querySelectorAll(".hex-code");
-      const spans = document.querySelectorAll(".num");
-      const colorName = document.querySelector(".color-name");
+      const palettes = document.querySelectorAll('.color-palette-item') as NodeListOf<HTMLElement>;
+      const hexes = document.querySelectorAll('.hex-code') as NodeListOf<HTMLElement>;
+      const spans = document.querySelectorAll('.num') as NodeListOf<HTMLElement>;
+      const colorName = document.querySelector('.color-name');
 
       palettes.forEach((item, i) => {
         item.style.backgroundColor = color[i];
-        item.addEventListener("click", (e) => {
-          rippleEffect(e, color[i]);
-        });
       });
 
       hexes.forEach((item, i) => {
@@ -29,115 +52,35 @@ const ColorPalette = () => {
         item.style.color = textcolor;
       });
 
-      colorName.innerHTML = colorNames[name];
+      if (colorName !== null) {
+        colorName.innerHTML = colorNames[name];
+      }
+
     };
 
-    const getContrast = (color) => {
-      if (color.slice(0, 1) === "#") {
+    const getContrast = (color: string) => {
+      if (color.slice(0, 1) === '#') {
         color = color.slice(1);
       }
 
-      const r = parseInt(color.substr(0, 2), 16);
-      const b = parseInt(color.substr(2, 2), 16);
-      const g = parseInt(color.substr(4, 2), 16);
+      const r = parseInt(color.substring(0, 2), 16);
+      const b = parseInt(color.substring(2, 2), 16);
+      const g = parseInt(color.substring(4, 2), 16);
 
       const yiq = (r * 299 + g * 587 + b * 114) / 1000;
-
-      return yiq >= 128 ? "black" : "white";
+      return yiq >= 128 ? 'black' : 'white';
     };
 
-    const LightenDarkenColor = (col, amt) => {
-      let usePound = false;
-
-      if (col[0] === "#") {
-        col = col.slice(1);
-        usePound = true;
-      }
-
-      let num = parseInt(col, 16);
-      let r = (num >> 16) + amt;
-
-      if (r > 255) r = 255;
-      else if (r < 0) r = 0;
-
-      let b = ((num >> 8) & 0x00ff) + amt;
-
-      if (b > 255) b = 255;
-      else if (b < 0) b = 0;
-
-      let g = (num & 0x0000ff) + amt;
-
-      if (g > 255) g = 255;
-      else if (g < 0) g = 0;
-
-      return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16);
-    };
-
-    const activeColor = (el) => {
-      const buttons = document.querySelectorAll(".color-button");
+    const activeColor = (el: HTMLInputElement) => {
+      const buttons = document.querySelectorAll('.color-button') as NodeListOf<HTMLElement>;
       buttons.forEach((item) => {
-        item.style.boxShadow = "none";
+        item.style.boxShadow = 'none';
       });
       const color = colors[el.name][6];
       el.style.boxShadow = `0px 0px 0px 2px var(--color-canvas), 0px 0px 0px 4px ${color}`;
     };
 
-    const rippleEffect = (event, color) => {
-      const button = event.currentTarget;
-      const ripple = document.createElement("i");
-      const diameter = Math.max(button.clientWidth, button.clientHeight);
-      const radius = diameter / 2;
-      const newColor = LightenDarkenColor(color, 25);
-      ripple.style.backgroundColor = newColor;
-      ripple.style.width = ripple.style.height = `${diameter}px`;
-      ripple.style.left = `${event.clientX - (button.offsetLeft + radius)}px`;
-      ripple.style.top = `${event.clientY - (button.offsetTop + radius)}px`;
-      ripple.classList.add("ripple");
-      const spans = document.querySelector(".ripple");
-      if (spans) {
-        spans.remove();
-      }
-      button.appendChild(ripple);
-    };
-
-    const copyText = () => {
-      const node = document.createElement("span");
-      const text = document.createTextNode("copied!");
-      node.classList.add("copied");
-      node.appendChild(text);
-      const nodes = document.querySelector(".copied");
-      if (nodes) {
-        nodes.remove();
-      }
-      return node;
-    };
-
-    const colorNames = {
-      red: "Red",
-      orange: "Orange",
-      amber: "Amber",
-      yellow: "Yellow",
-      lime: "Lime",
-      green: "Green",
-      emerald: "Emerald",
-      teal: "Teal",
-      cyan: "Cyan",
-      sky: "Sky",
-      blue: "Blue",
-      indigo: "Indigo",
-      violet: "Violet",
-      purple: "Purple",
-      magenta: "Magenta",
-      pink: "Pink",
-      rose: "Rose",
-      slate: "Slate",
-      zinc: "Zinc",
-      stone: "Stone",
-      gray: "Gray",
-      neutral: "Neutral",
-    };
-
-    const colors = {
+    const colors: ColorMap = {
       red:     [ '#FEF2F2', '#FEE2E2', '#FECACA', '#FCA5A5', '#F87171', '#EF4444', '#DC2626', '#B91C1C', '#991B1B', '#7F1D1D', '#450A0A', ],
       orange:  [ '#FFF7ED', '#FFEDD5', '#FED7AA', '#FDBA74', '#FB923C', '#F97316', '#EA580C', '#C2410C', '#9A3412', '#7C2D12', '#431407', ],
       amber:   [ '#FFFBEB', '#FEF3C7', '#FDE68A', '#FCD34D', '#FBBF24', '#F59E0B', '#D97706', '#B45309', '#92400E', '#78350F', '#451A03', ],
@@ -159,88 +102,33 @@ const ColorPalette = () => {
       neutral: [ '#FAFAFA', '#F5F5F5', '#E5E5E5', '#D4D4D4', '#A3A3A3', '#737373', '#525252', '#404040', '#262626', '#171717', '#0A0A0A', ],
     };
 
-    buttons.forEach((button) => {
-      button.addEventListener("click", () => {
-        selectColor(button.name);
-        activeColor(button);
+    buttons.forEach((button: Element) => {
+      button.addEventListener('click', () => {
+        selectColor((button as HTMLButtonElement).name);
+        activeColor(button as HTMLInputElement);
       });
     });
   }, []);
 
   return (
-    <div className="color-palette-container">
-      <div className="color-picker">
-        <button className="color-button" name="red" />
-        <button className="color-button" name="orange" />
-        <button className="color-button" name="amber" />
-        <button className="color-button" name="yellow" />
-        <button className="color-button" name="lime" />
-        <button className="color-button" name="green" />
-        <button className="color-button" name="emerald" />
-        <button className="color-button" name="teal" />
-        <button className="color-button" name="cyan" />
-        <button className="color-button" name="sky" />
-        <button className="color-button" name="blue" />
-        <button className="color-button" name="indigo" />
-        <button className="color-button" name="violet" />
-        <button className="color-button" name="purple" />
-        <button className="color-button" name="magenta" />
-        <button className="color-button" name="pink" />
-        <button className="color-button" name="rose" />
-        <button className="color-button" name="slate" />
-        <button className="color-button" name="neutral" />
+    <div className='color-palette-container'>
+      <div className='color-picker'>
+        {Object.keys(colorNames).map((item) => (
+          <button key={item} className='color-button' name={item.toLowerCase()} />
+        ))}
       </div>
-      <div className="color-chart">
-        <p className="color-name">Click on a bubble to see its color palette</p>
-        <div className="color-palette">
-          <div className="color-palette-item">
-            <span className="num">50</span>
-            <span className="hex-code" />
-          </div>
-          <div className="color-palette-item">
-            <span className="num">100</span>
-            <span className="hex-code" />
-          </div>
-          <div className="color-palette-item">
-            <span className="num">200</span>
-            <span className="hex-code" />
-          </div>
-          <div className="color-palette-item">
-            <span className="num">300</span>
-            <span className="hex-code" />
-          </div>
-          <div className="color-palette-item">
-            <span className="num">400</span>
-            <span className="hex-code" />
-          </div>
-          <div className="color-palette-item">
-            <span className="num">500</span>
-            <span className="hex-code" />
-          </div>
-          <div className="color-palette-item">
-            <span className="num">600</span>
-            <span className="hex-code" />
-          </div>
-          <div className="color-palette-item">
-            <span className="num">700</span>
-            <span className="hex-code" />
-          </div>
-          <div className="color-palette-item">
-            <span className="num">800</span>
-            <span className="hex-code" />
-          </div>
-          <div className="color-palette-item">
-            <span className="num">900</span>
-            <span className="hex-code" />
-          </div>
-          <div className="color-palette-item">
-            <span className="num">950</span>
-            <span className="hex-code" />
-          </div>
+      <div className='color-chart'>
+        <p className='color-name'>Click on a bubble to see its color palette</p>
+        <div className='color-palette'>
+          {colorValues.map((item) => (
+            <div key={item} className='color-palette-item'>
+              <span className='num select-none'>{item}</span>
+              <span className='hex-code' />
+            </div>
+          ))}
         </div>
       </div>
     </div>
-
   );
 };
 
